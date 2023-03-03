d524d431d0f102673f956e94c233867ac6b08968
Emil Malmsten <e.l.malmsten@student.tudelft.nl>
2023-03-03 10:57:54 +0100

    auto stack install

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from runner import *
 from tests import *
+import os
 
 all_tests = {}
 
@@ -8,5 +9,11 @@ all_tests.update(identity_tests)
 all_tests.update(object_index_tests)
 all_tests.update(array_tests)
 
+# To automatically run stack install before testing uncomment the following line, and change the path to the one
+#   containing your fp project (from the root of your home directory). Then uncomment line 18 in this file
+# fp_path = os.path.join(os.environ["HOMEPATH"], YOUR_FP_DIRECTORY_FROM_HOME)
+# https://www.geeksforgeeks.org/python-os-path-join-method/
+
 if __name__ == '__main__':
+    # os.system(f"cd {fp_path} && stack install")
     run_all(all_tests)
